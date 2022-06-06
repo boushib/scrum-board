b06b6d5197a5c5e282dfdc2d773ca77e9359420f
El Hassane Boushib <boushib.elhassane@gmail.com>
2022-06-06 21:35:44 +0100

    Compute board col number of stories

--- src/components/BoardCol/BoardCol.tsx
+++ src/components/BoardCol/BoardCol.tsx
@@ -9,7 +9,9 @@ interface Props {
 
 const BoardCol = ({ title, stories }: Props) => (
   <div className="board-col">
-    <div className="board-col__title">{title} - 12 issues</div>
+    <div className="board-col__title">
+      {title} - {stories.length} issue{stories.length === 1 ? '' : 's'}
+    </div>
     <hr />
     {stories.map((s) => (
       <StoryOverview story={s} key={s.id} />
