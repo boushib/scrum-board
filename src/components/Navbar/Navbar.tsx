b0c64bbe20edc5e053b5d2534bcac401dae5500e
El Hassane Boushib <boushib.elhassane@gmail.com>
2022-06-08 01:28:52 +0100

    Show user avatar in navabr if user is logged in

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import Button from '../Button'
+// import Button from '../Button'
 import './Navbar.sass'
 
 const Navbar = () => (
@@ -6,7 +6,13 @@ const Navbar = () => (
     <div className="container nav__container">
       <div className="nav__logo">Scrumify</div>
       <div className="nav__tail">
-        <Button>Login</Button>
+        {/* <Button>Login</Button> */}
+        <div
+          style={{
+            backgroundImage: `url("https://images.unsplash.com/photo-1615109398623-88346a601842?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80")`,
+          }}
+          className="nav__user"
+        ></div>
       </div>
     </div>
   </nav>
